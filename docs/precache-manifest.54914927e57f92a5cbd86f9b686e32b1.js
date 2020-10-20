6d5a50e1fc373bdfc763070a025d3afec413a16b
Felipe Beleño <Felipebelenoo1101@gmail.com>
2020-10-19 23:13:09 -0500

    Second Push and Commit

---
+++ docs/precache-manifest.54914927e57f92a5cbd86f9b686e32b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e21f5609c3fb674c41a248349e7b50a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a45e1f7b0c3fa40918",
+    "url": "/static/css/main.1dae77e7.chunk.css"
+  },
+  {
+    "revision": "e3f7aa1600adf4f83a0b",
+    "url": "/static/js/2.bdb478ab.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.bdb478ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a45e1f7b0c3fa40918",
+    "url": "/static/js/main.56ad5626.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
